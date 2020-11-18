25afd89ee49aee9f38656882895b7cd4b390af80
uzehhe <you@example.com>
2020-11-18 11:30:48 -0300

    add first GET route

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require ('express');
 const app =  express();
 
-
+const Datauser = [
+    {name: 'Jose'},
+    {name: 'Jill'}
+]
+app.get('/usuarios', (req, res) => {
+        res.json({
+            sucesso: 'verdadeiro',
+            mensagem : 'usuarios obtidos com sucesso',
+            usuarios: 'mockuserdata'
+        })
+})
 
 
 app.listen(3000,() =>{
